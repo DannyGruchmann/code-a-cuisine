ff5e108a218233ce577c403c625e0c5613538cdb
Danny Gruchmann <danny.grmn@icloud.com>
2026-01-03 22:09:32 +0100

    add landingpage

---
+++ src/app/landingpage/landingpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landingpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './landingpage.component.html',
+  styleUrls: ['./landingpage.component.scss','./landingpage_responisve.component.scss']
+})
+export class LandingpageComponent {
+
+}
