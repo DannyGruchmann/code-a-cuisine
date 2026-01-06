0b771550168bb97c973704a6f4480156a46f455a
Danny Gruchmann <danny.grmn@icloud.com>
2026-01-06 17:31:33 +0100

    add go back in logo

--- src/app/header-green-logo/header-green-logo.component.ts
+++ src/app/header-green-logo/header-green-logo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-green-logo',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header-green-logo.component.html',
   styleUrls: ['./header-green-logo.component.scss']
 })
