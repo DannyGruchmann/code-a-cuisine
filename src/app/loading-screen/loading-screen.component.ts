f892d11beac64f2842297475360479f87b629767
Danny Gruchmann <danny.grmn@icloud.com>
2026-01-17 21:03:59 +0700

    add better animation

--- src/app/loading-screen/loading-screen.component.ts
+++ src/app/loading-screen/loading-screen.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HeaderCreamLogoComponent } from '../header-cream-logo/header-cream-logo.component';
 
 @Component({
   selector: 'app-loading-screen',
   standalone: true,
   imports: [HeaderCreamLogoComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './loading-screen.component.html',
   styleUrls: ['./loading-screen.component.scss']
 })
